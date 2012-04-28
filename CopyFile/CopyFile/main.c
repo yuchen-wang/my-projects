d987844941c80cd3ca5652487a552f97e108b8da
Yuchen Wang <yuchen.wang.415@gmail.com>
2012-04-28 18:12:21 -0400

    First project

---
+++ CopyFile/CopyFile/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  CopyFile
+//
+//  Created by Yuchen Wang on 12-04-28.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
