a6ac6663b8a29f93b2158260b7cba8959e4ffbee
Yuchen Wang <yuchen.wang.415@gmail.com>
2012-04-29 19:41:53 -0400

    Learning Objective C

---
+++ Learnig Objective C/Learnig Objective C/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Learnig Objective C
+//
+//  Created by Yuchen Wang on 12-04-29.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
