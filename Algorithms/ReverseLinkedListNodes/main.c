95e71fa4a359eec2ec73ba6167e09278615be4a6
Yuchen Wang <yuchen.wang.415@gmail.com>
2014-02-19 23:28:03 -0500

    implement reverse k nodes in linked list

---
+++ Algorithms/ReverseLinkedListNodes/main.c
@@ -0,0 +1,74 @@
+//
+//  main.c
+//
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct ListNode ListNode;
+
+struct ListNode
+{
+    int n;
+    ListNode* next;
+};
+
+void reverseLinkedListNodes(ListNode* n, int k)
+{
+    ListNode* head = NULL;
+    ListNode* current = n;
+    ListNode* temp = NULL;
+    ListNode* tail = NULL;
+    ListNode* tempTail = NULL;
+
+    int i;
+    while (current != NULL) {
+        tempTail = current;
+        for (i = 0; i < k; i++) {
+            temp = current->next;
+            current->next = head;
+            head = current;
+            current = temp;
+            if (current == NULL) {
+                break;
+            }
+        }
+        if (tail != NULL) {
+            tail->next = head;
+        }
+        tail = tempTail;
+        head = NULL;
+    }
+}
+
+void printList(ListNode* list)
+{
+    if (list == NULL) {
+        printf("empty list\n");
+    } else {
+        printf("list: %d", list->n);
+        list = list->next;
+        while (list != NULL) {
+            printf(" -> %d", list->n);
+            list = list->next;
+        }
+        printf("\n");
+    }
+}
+
+int main(int argc, const char * argv[])
+{
+    ListNode f = {.n = 6, .next = NULL};
+    ListNode e = {.n = 5, .next = &f};
+    ListNode d = {.n = 4, .next = &e};
+    ListNode c = {.n = 3, .next = &d};
+    ListNode b = {.n = 2, .next = &c};
+    ListNode a = {.n = 1, .next = &b};
+
+    reverseLinkedListNodes(&a, 3);
+    printList(&c);
+    return 0;
+}
+
